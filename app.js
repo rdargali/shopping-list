28189b856e0115262a5125ec0549ec67cd928e87
Rawand Dargali <rdargali@yahoo.com>
2020-08-02 01:01:46 -0500

    add shopping list page created and form posting to db

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log("Server is running");
-});
--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const shoppingList = require("./models/shoppingList");
 const groceryItem = require("./models/groceryItem");
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

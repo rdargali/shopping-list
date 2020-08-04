e1633fe2a48e08a7015a1bdcd69a5f4992b47058
Rawand Dargali <rdargali@yahoo.com>
2020-08-04 01:25:03 -0500

    posting grocery items working

--- client/add-grocery-item.js
+++ client/add-grocery-item.js
@@ -1,4 +1,5 @@
 const VIEW_ALL_SHOPPING_LIST_URL = "http://localhost:3000/api/shopping-list";
+const ADD_GROCERY_ITEM_URL = "http://localhost:3000/api/grocery-items";
 
 const shoppingListDropDownMenu = document.querySelector(
   "#shoppingListDropDownMenu"
@@ -8,7 +9,39 @@ const shoppingListDropDownButton = document.querySelector(
   "#shoppingListDropDownButton"
 );
 
+const nameTextBox = document.querySelector("#nameTextBox");
+const priceTextBox = document.querySelector("#priceTextBox");
+const quantityTextBox = document.querySelector("#quantityTextBox");
+const saveGroceryItemButton = document.querySelector("#saveGroceryItem");
 let selectedShoppingListId = "";
+
+saveGroceryItemButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  saveGroceryItem();
+});
+
+const saveGroceryItem = () => {
+  const name = nameTextBox.value;
+  const price = parseFloat(priceTextBox.value);
+  const quantity = parseInt(quantityTextBox.value);
+
+  fetch(ADD_GROCERY_ITEM_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      price: price,
+      quantity: quantity,
+      shoppingListId: selectedShoppingListId,
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      window.location = "view-all-shopping-lists.html";
+    });
+};
 
 const selectShoppingList = (SLId, name) => {
   shoppingListDropDownButton.innerHTML = name;
